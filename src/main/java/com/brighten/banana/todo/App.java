7df7e10de99f42676a7d530f8105c352916d3328
netkid7 <netkid9@gmail.com>
2019-05-02 22:24:50 +0900

    code format

--- src/main/java/com/brighten/banana/todo/App.java
+++ src/main/java/com/brighten/banana/todo/App.java
@@ -10,18 +10,18 @@ import com.brighten.banana.todo.service.GreetingMessageService;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        
-        GreetingMessageService greetingMessageService = context.getBean("greetingMessageServiceImpl", GreetingMessageService.class);
-        System.out.println(greetingMessageService.greetUser());
-        
-        Knight knight = (Knight)context.getBean("knight");
-        knight.embarkOnQuest();
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+		GreetingMessageService greetingMessageService = context.getBean("greetingMessageServiceImpl",
+				GreetingMessageService.class);
+		System.out.println(greetingMessageService.greetUser());
+
+		Knight knight = (Knight) context.getBean("knight");
+		knight.embarkOnQuest();
+	}
 }
